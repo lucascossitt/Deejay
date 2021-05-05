9db49805f61de7720a6515671d11512eeefd81f8
Luquinhass <lucascossitt@hotmail.com>
2021-05-05 14:20:58 -0300

    add

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ module.exports = {
     'prefix':'-',
     'owner':'227457062643171338',
     'errorChannel':'784121542408667136',
-    'logsChannel':'784121606511132732',
     'comandosChannel': '784121560776704080'
 }
