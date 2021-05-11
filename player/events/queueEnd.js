100915eaa7227b90dec13009020d6176e33fb6b1
Luquinhass <lucascossitt@hotmail.com>
2021-05-10 22:26:06 -0300

    fix

--- player/events/queueEnd.js
+++ player/events/queueEnd.js
@@ -3,7 +3,7 @@ module.exports = async(client, player) => {
     const embed = new Discord.MessageEmbed()
     .setColor('#000000')
     .setTimestamp()
-    .setDescription(`A fila de musicas acabou, caso queira escutar mais utilize \`lm.play\``)
+    .setDescription(`A fila de musicas acabou, caso queira escutar mais utilize \`-play\``)
     client.channels.cache.get(player.textChannel).send(embed)
     player.destroy()
 }
