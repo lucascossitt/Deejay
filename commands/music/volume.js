101614f9d5b099d94ca5c1f0b26435de1650f465
lucascossitt <lucascossitt@inovaclick.com.br>
2021-09-07 18:32:05 -0300

    alteraçoes

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -28,7 +28,7 @@ module.exports = class Volume extends Command{
             } else {
                 if(!message.member.roles.cache.get(message.guildDb.djRole) && !message.author.id === client.config.owner && message.author.id !== player.queue.current.requester.id && message.guild.channels.cache.get(player.voiceChannel).members.filter(a => !a.user.bot).size > 1) return message.quote(`Somente DJ's e quem colocou a musica podem alterar o volume.`)
                 if(isNaN(volume)) return message.quote('Coloque um volume entre 1 e 100.')
-                if(volume > 100 && !message.author.id === client.config.owner) return message.quote('Coloque um volume entre 1 e 100.')
+                if(volume > 100) return message.quote('Coloque um volume entre 1 e 100.')
                 if(volume < 1) return message.quote('Coloque um volume entre 1 e 100.')
 
                 player.setVolume(volume)
